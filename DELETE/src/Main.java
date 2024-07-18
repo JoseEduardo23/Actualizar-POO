24b7ca8f0686f1eb7c1e159df19008368a7ae742
Eduardo <joselincango73@gmail.com>
2024-07-18 08:16:23 -0500

    Eliminacion de datos usando DELETE para la base de datos

---
+++ DELETE/src/Main.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/estudiantes";
+        String usuario = "root";
+        String password = "j.eduardo23";
+
+        String sql = "DELETE FROM estudiantes WHERE Cedula_EST = ?";
+        PreparedStatement preparedStatement = null;
+        Connection connection = DriverManager.getConnection(url, usuario, password);
+        String cedula = "0338943111";
+        try{
+            System.out.println("Conexión con la base de datos");
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, cedula);
+
+            int filasEliminadas = preparedStatement.executeUpdate();
+            if (filasEliminadas>0){
+                System.out.println("El estudiante con cedula " + cedula + " se ha eliminado correctamente" );
+            }else {
+                System.out.println("No se ha encontrado ningun estudiante con cedula " + cedula);
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo conectar con la base de datos");
+            throw new RuntimeException(e);
+        } finally {
+            // Cerrar la conexión
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection!=null){
+                    connection.close();
+                }
+            } catch (SQLException e1) {
+                System.out.println(e1.getMessage());
+            }
+        }
+    }
+}
